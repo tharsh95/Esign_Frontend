148986094549ff22ff12c532986c311820777bca
Harsh Tiwari <harsh@creditap.ai>
2023-10-23 15:42:14 +0530

    esign

--- src/components/pages/CreateEsign.jsx
+++ src/components/pages/CreateEsign.jsx
@@ -2,35 +2,39 @@ import React, { useRef, useState } from "react";
 import {
   Card,
   CardContent,
-  TextField,
   Button,
   Typography,
   Grid,
-  styled,
 } from "@mui/material";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
+import * as FormData from "form-data";
+
 import axios from "axios";
 
 const CreateEsign = () => {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
 
   const handleUpload = (e) => {
     setFile(e.target.files[0]);
-  }
+  };
 
   const handleSubmit = async () => {
     if (file) {
       const formData = new FormData();
       formData.append("file", file);
-      const url = 'http://localhost:3000/pdf/upload'
-      await axios.post(url, formData, {
+      const url = "http://localhost:3000/pdf/upload";
+      const { data } = await axios.post(url, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data', // Set the content type
+          "Content-Type": "multipart/form-data",
         },
-      })
+      });
+      data.status === "failure"
+        ? enqueueSnackbar(data.message, { variant: "error" })
+        : enqueueSnackbar(data.message, { variant: "success" });
     } else {
-      console.error('No file selected.');
+      console.error("No file selected.");
     }
-  }
+  };
   return (
     <Grid
       container
@@ -46,22 +50,18 @@ const CreateEsign = () => {
             <Typography variant="h6" gutterBottom>
               Create E-sign
             </Typography>
-            <input
-              type="file"
-              onChange={handleUpload}
-            />
+            <input type="file" onChange={handleUpload} />
           </CardContent>
         </Card>
       </Grid>
       <Grid item>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSubmit}
-        >
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
           Upload
         </Button>
       </Grid>
+      <div>
+        <SnackbarProvider />
+      </div>
     </Grid>
   );
 };
--- src/components/pages/ViewSign..jsx
+++ src/components/pages/ViewSign..jsx
@@ -7,37 +7,23 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import axios from "axios";
-import { SnackbarProvider, enqueueSnackbar } from 'notistack';
-
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
+import moment from "moment";
 import { Button, Dialog, DialogContent, Tooltip } from "@mui/material";
-// const data = [
-//   {
-//     "id": 9,
-//     "owner_id": "50200000000028003",
-//     "request_id": "50200000000031151",
-//     "document_id": "50200000000031152",
-//     "action_id": "50200000000031168",
-//     "createdAt": "2023-10-20T15:01:00.000Z",
-//     "updatedAt": "2023-10-20T15:01:00.000Z",
-//   },
-//   {
-//     "id": 11,
-//     "owner_id": "50200000000028003",
-//     "request_id": "50200000000031179",
-//     "document_id": "50200000000031180",
-//     "action_id": "50200000000031196",
-//     "createdAt": "2023-10-20T15:37:07.000Z",
-//     "updatedAt": "2023-10-20T15:37:07.000Z",
-//   }
-// ];
 
-const MyTable = () => {
+const ViewEsign = () => {
   const [data, setData] = useState([]);
   const [open, setOpen] = useState(false);
   const [previewData, setPreviewData] = useState(null);
+  const [flag,setFlag]=useState(false)
 
   const fetchData = async () => {
-    const { data } = await axios.get("http://localhost:3000/pdf/list");
+    const {
+      data: { data, message,status },
+    } = await axios.get("http://localhost:3000/pdf/list");
+    status==='success'?
+    enqueueSnackbar(message, { variant: "info" }):
+    enqueueSnackbar(message, { variant: "error" })
     const groupedArray = data.reduce((acc, curr) => {
       const existingGroup = acc.find(
         (item) =>
@@ -57,7 +43,6 @@ const MyTable = () => {
 
       return acc;
     }, []);
-    // console.log(groupedArray[0].meta.requests.actions[0].recipient_email);
     setData([groupedArray]);
   };
 
@@ -71,12 +56,19 @@ const MyTable = () => {
   };
 
   const handleUpdate = async (requestId) => {
-    const { data } = await axios.put(`http://localhost:3000/pdf/update/${requestId}`);
-    enqueueSnackbar(data.message)
+    const { data } = await axios.put(
+      `http://localhost:3000/pdf/update/${requestId}`
+    );
+    setFlag(!flag)
+    data.status === "success"
+      ? enqueueSnackbar(data.message, { variant: "success" })
+      : enqueueSnackbar(data.message, { variant: "error" });
   };
   const handleSubmit = async (id) => {
     const { data } = await axios.post(`http://localhost:3000/pdf/${id}/submit`);
-    enqueueSnackbar(data.message)
+    data.status === "success"
+      ? enqueueSnackbar(data.message, { variant: "success" })
+      : enqueueSnackbar(data.message, { variant: "warning" });
   };
   useEffect(() => {
     fetchData();
@@ -91,22 +83,19 @@ const MyTable = () => {
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
-              {/* <TableCell>Owner ID</TableCell> */}
               <TableCell>Request ID</TableCell>
               <TableCell>Status</TableCell>
               <TableCell>Email/s</TableCell>
-              {/* <TableCell>Created At</TableCell> */}
               <TableCell>Preview</TableCell>
               <TableCell>Update</TableCell>
               <TableCell>Submit</TableCell>
+              <TableCell>Created At</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {data[0]?.map((row) => (
               <TableRow key={row.id}>
-                {/* {console.log(row.meta.requests.document_ids[0].image_string)} */}
                 <TableCell>{row.id}</TableCell>
-                {/* <TableCell>{row.owner_id}</TableCell> */}
                 <TableCell>{row.request_id}</TableCell>
                 <TableCell>
                   {row.status !== "completed" ? (
@@ -119,10 +108,9 @@ const MyTable = () => {
                 </TableCell>
                 <TableCell>
                   {row.meta?.requests.actions.map((el) => (
-                    <div>{el.recipient_email} </div>
+                    <div key={el.recipient_email}>{el.recipient_email} </div>
                   ))}
                 </TableCell>
-                {/* <TableCell>{row.createdAt}</TableCell> */}
                 <TableCell>
                   <Button
                     onClick={() =>
@@ -141,6 +129,7 @@ const MyTable = () => {
                     onClick={() => handleUpdate(row.request_id)}
                     variant="contained"
                     color="primary"
+                    disabled={row.status==='inprogress'||row.status==='completed'}
                   >
                     Update
                   </Button>
@@ -154,6 +143,7 @@ const MyTable = () => {
                     Submit
                   </Button>
                 </TableCell>
+                <TableCell>{moment(row.createdAt).format("llll")}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -170,10 +160,9 @@ const MyTable = () => {
       </Dialog>
       <div>
         <SnackbarProvider />
-        {/* <button onClick={() => enqueueSnackbar('That was easy!')}>Show snackbar</button> */}
       </div>
     </Fragment>
   );
 };
 
-export default MyTable;
+export default ViewEsign;
